716d5ba1fb3f8f9228cd3d4875934de9534e8ee1
Chen <1418621447@qq.com>
2021-10-19 11:12:14 +0800

    Initial commit

---
+++ src/main/java/com/rounds/experimentalteachingsystm/ExperimentalTeachingSystmApplication.java
@@ -0,0 +1,13 @@
+package com.rounds.experimentalteachingsystm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentalTeachingSystmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentalTeachingSystmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rounds/experimentalteachingsystm/ExperimentalTeachingSystmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rounds.experimentalteachingsystm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentalTeachingSystmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
