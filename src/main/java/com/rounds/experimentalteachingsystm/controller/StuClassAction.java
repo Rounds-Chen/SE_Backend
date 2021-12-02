a741884d37776de56db59d167c40146c40f12f2c
PinkCrow007 <1053603622@qq.com>
2021-12-02 21:53:58 +0800

    ...

--- src/main/java/com/rounds/experimentalteachingsystm/controller/StuClassAction.java
+++ src/main/java/com/rounds/experimentalteachingsystm/controller/StuClassAction.java
@@ -1,6 +1,12 @@
 package com.rounds.experimentalteachingsystm.controller;
 
 
+import com.rounds.experimentalteachingsystm.entity.StuClassEntity;
+import com.rounds.experimentalteachingsystm.service.CourseService;
+import com.rounds.experimentalteachingsystm.service.StuClassService;
+import com.rounds.experimentalteachingsystm.util.AjaxJson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("//stuClassEntity")
 public class StuClassAction {
+    @Autowired
+    StuClassService stuClassService;
 
+    @PostMapping("//postStuClass")
+    AjaxJson postStuClass(StuClassEntity stuClassEntity){
+        return AjaxJson.getSuccessData(stuClassService.save(stuClassEntity));
+    }
+    @PostMapping("//updateStuClass")
+    AjaxJson updateStuClass(StuClassEntity stuClassEntity){
+        boolean flg=stuClassService.updateById(stuClassEntity);
+        if(flg) return AjaxJson.getSuccess();
+        else return  AjaxJson.getError();
+    }
 }
 
