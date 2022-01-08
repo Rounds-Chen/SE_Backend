d696cba266f172a605847c6996fd78f46a692570
Chen <1418621447@qq.com>
2022-01-08 21:24:04 +0800

    feat: 上传实验报告；获取所有实验报告

--- src/main/java/com/rounds/experimentalteachingsystm/controller/AucReportAction.java
+++ src/main/java/com/rounds/experimentalteachingsystm/controller/AucReportAction.java
@@ -1,7 +1,10 @@
 package com.rounds.experimentalteachingsystm.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
+import com.rounds.experimentalteachingsystm.entity.AucReport2Entity;
 import com.rounds.experimentalteachingsystm.entity.AucReportEntity;
+import com.rounds.experimentalteachingsystm.service.AucReport2Service;
 import com.rounds.experimentalteachingsystm.service.AucReportService;
 import com.rounds.experimentalteachingsystm.service.FileStorageService;
 import com.rounds.experimentalteachingsystm.util.AjaxJson;
@@ -10,15 +13,17 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.UnsupportedEncodingException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -39,35 +44,44 @@ public class AucReportAction {
     @Autowired
     AucReportService aucReportService;
 
-    /**
-     * 上传实验报告
-     * @param stuId 学生id
-     * @param aucId 拍卖id
-     * @param file 文件
-     * @return
-     */
+    @Autowired
+    AucReport2Service aucReport2Service;
+
+
+    @RequestMapping("//getReports")
+    @ApiOperation(value = "获取所有实验报告")
+    AjaxJson getReports(){
+        try {
+            List<AucReport2Entity> res = aucReport2Service.list();
+            return AjaxJson.getSuccessData(res);
+        }catch (Exception e){
+            return AjaxJson.getError(e.toString());
+        }
+    }
+
     @PostMapping("//postReport")
     @ApiOperation(value="提交报告")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="stuId",value = "学生id",dataType = "String"),
-            @ApiImplicitParam(name="aucId",value = "拍卖id",dataType = "Integer")
+            @ApiImplicitParam(name="body",value = "页面内容",dataType = "String")
     })
-    AjaxJson submitReport( String stuId, Integer aucId, MultipartFile file){
-            String filePath=fileStorageService.upload(file);
-            String suffix=file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+    AjaxJson submitReport(@RequestBody String body) throws UnsupportedEncodingException, SQLException {
+        JSONObject req=JSONObject.parseObject(body);
+        String name=req.getString("name");
+        String id=req.getString("id");
+        Blob content=new SerialBlob(req.getString("content").getBytes("GBK"));
 
-//            Map<String,String> ans=new HashMap<>();
-            if(null!=filePath){
-                AucReportEntity entity=new AucReportEntity();
-                entity.setAuctionId(aucId);
-                entity.setStudentId(stuId);
-                entity.setFileUrl(filePath);
+        AucReport2Entity entity=new AucReport2Entity();
+        entity.setContent(content);
+        entity.setId(id);
+        entity.setName(name);
 
-//                aucReportService.save(entity);
-                return AjaxJson.getSuccess();
-            }else{
-                return AjaxJson.getError();
-            }
+        try{
+            aucReport2Service.save(entity);
+            return AjaxJson.getSuccess();
+        }catch (Exception e){
+            return AjaxJson.getError(e.toString());
+        }
+
     }
 
 }
